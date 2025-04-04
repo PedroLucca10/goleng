423e7907ba3e1a0bea47e1840c5211adc208f392
Pedro <pedro.l.anjos7@aluno.senai.br>
2025-04-04 07:50:37 -0300

    Vetor de superherois

--- main.go
+++ main.go
@@ -9,4 +9,21 @@ func main (){
      fmt.Println(nomes)
      nomes[2] = "Pedro"
      fmt.Println(nomes)
+
+     var score = []int{100, 200, 300, 400}
+     fmt.Println(score)
+     score[1] = 2
+     fmt.Println(score, len(score), cap(score))
+     rangeOne := score[1:3]
+     fmt.Println(rangeOne)
+     rangeTwo := score[2:]
+     fmt.Println(rangeTwo)
+     rangeThree := score [:3]
+     fmt.Println(rangeThree)
+
+
+     var superherois = []string{"Batman", "Homem-aranha", "Ben 10"}
+     fmt.Println(superherois)
+     superherois = append(superherois, "Capitão América")
+     fmt.Println(superherois, len(superherois), cap(superherois))
 }
