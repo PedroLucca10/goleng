4104181bfc193b7613329b5350dcb255f2145d73
Pedro <pedro.l.anjos7@aluno.senai.br>
2025-04-11 14:45:54 -0300

    Programa Bancário simples

--- main.go
+++ main.go
@@ -2,71 +2,50 @@ package main
 
 import "fmt"
 
-var saldo float64
-
 func main() {
+    var saldo float64
+    var opcao int
+    var valor float64
+
     fmt.Println("Digite seu saldo inicial: R$")
     fmt.Scan(&saldo)
 
     for {
-        exibirMenu()
-        opcao := lerOpcao()
+        fmt.Println("Saldo atual: R$", saldo)
+        fmt.Println("1. Depositar")
+        fmt.Println("2. Sacar")
+        fmt.Println("3. Sair")
+        fmt.Println("➔ Escolha uma opção:")
+        fmt.Scan(&opcao)
 
         if opcao == 1 {
-            depositar()
+            fmt.Println("Valor para depositar: R$")
+            fmt.Scan(&valor)
+            if valor > 0 {
+                saldo += valor
+                fmt.Println("Depósito de R$", valor, "realizado!")
+            } else {
+                fmt.Println("Valor inválido!")
+            }
+
         } else if opcao == 2 {
-            sacar()
+            fmt.Println("Valor para sacar: R$")
+            fmt.Scan(&valor)
+            if valor > saldo {
+                fmt.Println("Saldo insuficiente!")
+            } else if valor <= 0 {
+                fmt.Println("Valor inválido!")
+            } else {
+                saldo -= valor
+                fmt.Println("Saque de R$", valor, "realizado!")
+            }
+
         } else if opcao == 3 {
-            encerrar()
+            fmt.Println("Encerrando... Saldo final:", saldo)
             break
+
         } else {
             fmt.Println("Opção inválida! Use 1, 2 ou 3.")
         }
     }
-}
-
-func exibirMenu() {
-    fmt.Println("Saldo atual: R$", saldo)
-    fmt.Println("1. Depositar")
-    fmt.Println("2. Sacar")
-    fmt.Println("3. Sair")
-    fmt.Println("Escolha uma opção:")
-}
-
-func lerOpcao() int {
-    var opcao int
-    fmt.Scan(&opcao)
-    return opcao
-}
-
-func depositar() {
-    fmt.Println("Valor para depositar: R$")
-    var valor float64
-    fmt.Scan(&valor)
-
-    if valor > 0 {
-        saldo += valor
-        fmt.Println("Depósito de R$", valor, "realizado!")
-    } else {
-        fmt.Println("Valor inválido!")
-    }
-}
-
-func sacar() {
-    fmt.Println("Valor para sacar: R$")
-    var valor float64
-    fmt.Scan(&valor)
-
-    if valor > saldo {
-        fmt.Println("Saldo insuficiente!")
-    } else if valor <= 0 {
-        fmt.Println("Valor inválido!")
-    } else {
-        saldo -= valor
-        fmt.Println("Saque de R$", valor, "realizado!")
-    }
-}
-
-func encerrar() {
-    fmt.Println("Encerrando... Saldo final:", saldo)
 }
