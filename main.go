7ae01ead10bce90e35f3b42d1ff4c75a784c9ecd
Pedro <pedro.l.anjos7@aluno.senai.br>
2025-04-24 14:19:21 -0300

    Capitais

--- main.go
+++ main.go
@@ -3,29 +3,21 @@ package main
 import(
   "fmt"
 )
-var nota1 float64
-var nota2 float64
 
-func analisarNotas()(float64, string) {
-  fmt.Println("Qual sua 1ª nota: ")
-  fmt.Scan(&nota1)
-  fmt.Println("Qual sua 2ª nota: ")
-  fmt.Scan(&nota2)
-  var media = (nota1 + nota2) / 2
-  if media < 6 {
-    return media, "Reprovado"
-  } else if media < 0 || media > 10{
-    return media, "Nota inválida"
-  } else {
-    return media, "Aprovado"
-  }
-}
 
   func main() {
-    media, resultado := analisarNotas()
-    fmt.Println("Média: ", media)
-    fmt.Println("Resultado: ", resultado)
+      capitais := map[string][string] {
+        "SP" : "São Paulo",
+        "RJ" : "Rio  de janeiro"
+        "ES" : "Espírito Santo"
+      }
 
+      capitais ["BH"] = "Belo Horizonte"
+
+      for k, v := range capitais {
+        fmt.Println("Sigla, Nome", k, v)
+      }
+                  
   }
   
   
