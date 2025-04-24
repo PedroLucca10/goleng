ff23387c3f51c602d5dd6d25e924b3de0fc42b3e
Pedro <pedro.l.anjos7@aluno.senai.br>
2025-04-24 14:09:30 -0300

    AnalisarNotas

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
-package make
+package main
 
 import(
   "fmt"
 )
+var nota1 float64
+var nota2 float64
 
-func pegarNome()(string, string){
-  return "Barry", "Allen"
+func analisarNotas()(float64, string) {
+  fmt.Println("Qual sua 1ª nota: ")
+  fmt.Scan(&nota1)
+  fmt.Println("Qual sua 2ª nota: ")
+  fmt.Scan(&nota2)
+  var media = (nota1 + nota2) / 2
+  if media < 6 {
+    return media, "Reprovado"
+  } else if media < 0 || media > 10{
+    return media, "Nota inválida"
+  } else {
+    return media, "Aprovado"
+  }
 }
 
-func main(){
- nome, sobrenome := pegarNome()
- fmt.Println(nome)
- fmt.Println(sobrenome)
-}
+  func main() {
+    media, resultado := analisarNotas()
+    fmt.Println("Média: ", media)
+    fmt.Println("Resultado: ", resultado)
+
+  }
+  
+  
